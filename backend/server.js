0494fe74acb22ea6b6882199a03b501f9db609f8
AngelluzM <4NG3L0UC0@gmail.com>
2025-06-08 17:54:50 -0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -46,7 +46,7 @@ io.on('connection', (socket) => {
 });
 
 // Iniciar o servidor
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 server.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
 });
