1ba9e6de7749ff78845e8ff8f9750375b957490c
AngelluzM <4NG3L0UC0@gmail.com>
2025-06-20 19:11:35 -0300

    Update sala.js

--- frontend/js/sala.js
+++ frontend/js/sala.js
@@ -46,7 +46,7 @@ export function iniciarSala() {
     }
     setInterval(atualizarRelogio, 1000);
     atualizarRelogio();
-  });
+  
   
 	console.log('✅ Sidebar (sala) VISÍVEL!');
 
@@ -65,7 +65,7 @@ export function iniciarSala() {
       btnImportar.classList.add('hidden');
 	  btnAddPdf.classList.add('hidden');
     }
-  
+  });
 
   // Atualiza a lista de jogadores sempre com o HOST no topo
   socket.on('updatePlayerList', ({ jogadores }) => {
