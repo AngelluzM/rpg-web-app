030ff135d338fcf85e17209bcae88095311165ba
AngelluzM <4NG3L0UC0@gmail.com>
2025-07-06 10:39:04 -0300

    Update mapaSharing.js

--- backend/mapaSharing.js
+++ backend/mapaSharing.js
@@ -9,12 +9,6 @@ module.exports = function(io, salas) {
     // ...outros eventos: tokens, camadas, permissões
 
 
-    // Ao entrar numa sala, manda a imagem atual para o novo cliente
-    socket.on('joinRoom', ({ roomCode }) => {
-      const sala = salas[roomCode];
-      if (sala && sala.mapaImg) {
-        socket.emit('updateMapa', { imgBase64: sala.mapaImg });
-      }
-    });
+
   });
 };
